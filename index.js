87e9a7b37da29ec08c2dc99ec1f3aa985af734d8
Iqbal Ahmed <iqbalhussain9865@gmail.com>
2021-03-23 21:29:22 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req,res) => {
+    res.send('I know how to open node')
+})
+app.listen(3000, ()=>console.log('listening to port 3000'))
